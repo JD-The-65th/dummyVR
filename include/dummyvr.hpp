871a0399deb6ec3949a62aa43f11b4789f2607c7
JD Williams <jdwill239@gmail.com>
2022-06-04 19:40:45 -0400

    Warn the Windows User + Initiate Headset Class

--- include/dummyvr.hpp
+++ include/dummyvr.hpp
@@ -1,11 +1,13 @@
+#include <iostream>
+#include <string>
 namespace dummyVR {
 
-    int location[2][2][2];
-    void updateLoc(int x, int y, int z) {
-    location[1][0][0] = x;
-    location[0][1][0] = y;
-    location[0][0][1] = y;
+    class Headset {
+        public:
+        std::string headsetName;
+        std::string headsetType;
+        int location[2][2][2];
 
-    }
+    };
 
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <thread>
 #include <filesystem>
+#include <string>
 #include "dummyvr.hpp"
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined (__CYGWIN__)
+
+std::cout << "You're using windows, so results may vary. Don't come crying to us when the program goes belly up."
+std::string steamLocation = "C:\\Program Files\\Steam\\steamapps"
+#else
+std::string steamLocation = "~/.steam/steam/steamapps";
+#endif
+
+std::string steamVrDriverLoc = steamLocation + "/common/SteamVR/drivers";
+
 
 
 int main() {
 
-    dummyVR::updateLoc(0, 0, 0);
-
+    std::cout << steamVrDriverLoc;
     return 0;
 
 };
